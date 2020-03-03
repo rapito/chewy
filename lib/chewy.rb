15369d4dd67cbf5365c565f7e33a329cb50b1a58
Robert Peralta <robert.peralta@helloglobo.com>
2020-03-03 15:23:30 -0400

    Rollback

--- lib/chewy.rb
+++ lib/chewy.rb
@@ -195,7 +195,6 @@ module Chewy
     #   city3.do_update! # index updated again
     #
     def strategy(name = nil, &block)
-      byebug
       Thread.current[:chewy_strategy] ||= Chewy::Strategy.new
       if name
         if block
